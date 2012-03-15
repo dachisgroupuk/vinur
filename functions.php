93b4fef65df9cfe64fc88c1e45caf9d90fbf8557
digitales <ross.tweedie@headshift.com>
2012-03-15 12:15:50 +0000

    Changing sidebar headers to H3s from H1s

--- functions.php
+++ functions.php
@@ -90,8 +90,8 @@ function vinur_widgets_init() {
 		'id' => 'sidebar-1',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget' => "</aside>",
-		'before_title' => '<h1 class="widget-title">',
-		'after_title' => '</h1>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',            
 	) );
 }
 add_action( 'widgets_init', 'vinur_widgets_init' );
