d1a71283325111a3e1e1e70af8805457ef90d86e
digitales <ross.tweedie@headshift.com>
2012-03-16 11:40:05 +0000

    Changing the primary div to posts section

--- page.php
+++ page.php
@@ -13,19 +13,16 @@
 
 get_header(); ?>
 
-		<div id="primary" class="site-content">
-			<div id="content" role="main">
+<section id="posts">
 
-				<?php while ( have_posts() ) : the_post(); ?>
+    <?php while ( have_posts() ) : the_post(); ?>
 
-					<?php get_template_part( 'content', 'page' ); ?>
+        <?php get_template_part( 'content', 'page' ); ?>
 
-					<?php comments_template( '', true ); ?>
+        <?php comments_template( '', true ); ?>
 
-				<?php endwhile; // end of the loop. ?>
-
-			</div><!-- #content -->
-		</div><!-- #primary .site-content -->
-
+    <?php endwhile; // end of the loop. ?>
+</section>
+		
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
