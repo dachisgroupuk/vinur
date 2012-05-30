cc12065c9d146f2cc1a10b3bd3a3f7ea6236f8ea
digitales <ross.tweedie@headshift.com>
2012-05-30 18:19:47 +0100

    Updated theme

--- plugins/vinur_widgets.php
+++ plugins/vinur_widgets.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 //class Sharing_Widget
 require( get_template_directory() . '/plugins/widgets/sharing_widget.php' );
 //class Twitter_Widget
@@ -7,4 +7,3 @@ require( get_template_directory() . '/plugins/widgets/twitter_widget.php' );
 // register custom widgets
 add_action('widgets_init', create_function('', 'return register_widget("Sharing_Widget");'));
 add_action('widgets_init', create_function('', 'return register_widget("Twitter_Widget");'));
-?>
